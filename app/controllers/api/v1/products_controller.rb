9f5463e5d25be594c589fe7b5d8e8a7c9c1b74a2
Jonathan Canaveral <jonathan.canaveral@moneysmart.com>
2020-09-06 21:24:09 +0800

    search by nearby shops and product tags

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,13 +1,18 @@
 class Api::V1::ProductsController < ApplicationController
-  before_action :set_products, :serialize_products, only: :index
+  before_action :set_products,
+                :serialize_products,
+                :nearby_shops,
+                :product_tags, only: :index
 
   api :GET, '/v1/products'
   param :search, Hash, desc: 'filter search' do
     param :keywords, String, desc: "Search by product name or shop's name"
     param :nearby, Hash, desc: 'Search nearby' do
-      param :lat, Integer, desc: 'Latitude'
-      param :lon, Integer, desc: 'Longitude'
+      param :lat, Integer, desc: 'Latitude', required: true
+      param :lon, Integer, desc: 'Longitude', required: true
+      param :radius, Integer, desc: 'Radial width by kilometers', required: true
     end
+    param :tags, Array, desc: 'Array string of product tags'
   end
   def index
     render json: @serialize_products
@@ -17,9 +22,25 @@ class Api::V1::ProductsController < ApplicationController
 
   def set_products
     @products = Product.all.preload(:images, :shop)
+    @products.where(shop: @nearby_shops) if @nearby_shops.present?
+    @products.where(tags: @product_tags) if @product_tags.present?
   end
 
   def serialize_products
     @serialize_products = ProductSerializer.new(@products)
+  end
+
+  def nearby_shops
+    return if params[:nearby].blank?
+
+    coordinates = params[:nearby]
+    @nearby_shops = Shop.near(
+      [coordinates[:lot], coordinates[:lon]],
+      coordinates[:radius]
+    )
+  end
+
+  def product_tags
+    @product_tags = params[:tags] if params[:product_tags].present?
   end
 end
