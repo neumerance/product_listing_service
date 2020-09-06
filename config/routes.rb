2aa2b451029bd270a3c0d4c96e387a2d6bdb4f83
Jonathan Canaveral <jonathan.canaveral@moneysmart.com>
2020-09-06 19:39:17 +0800

    products endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  apipie
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      resources :products, only: :index
+    end
+  end
 end
